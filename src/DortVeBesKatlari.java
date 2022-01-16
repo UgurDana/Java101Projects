fb63b606231c7ea869f6403ad2901a77c6f48cd8
90536 <kanexsa>
2022-01-16 17:31:38 +0300

    DortVeBesKatlari cevabı

--- src/DortVeBesKatlari.java
+++ src/DortVeBesKatlari.java
@@ -1,14 +1,18 @@
 import java.util.Scanner;
+
 public class DortVeBesKatlari {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int number;
         System.out.println("Bir sayi giriniz : ");
         number = scanner.nextInt();
-        for(int i = 1, k = 1; i<=number || k <=number; i *= 4,k *=5) {
-            if(i != 1)
-                System.out.println(i);
-            System.out.println(k);
+        System.out.println("4 ün katları :");
+        for (int i = 1; i <= number; i *= 4) {
+            System.out.println(i);
+        }
+        System.out.println("5 in katları :");
+        for (int j = 1; j <= number; j *= 5) {
+            System.out.println(j);
         }
     }
 }
